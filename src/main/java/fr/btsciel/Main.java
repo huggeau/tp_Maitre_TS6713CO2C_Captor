1632cf33d513f1ff892ea9a26b7ae8900baabb3d
goisot <hugo.goisot@lyceeastier.com>
2024-03-25 15:34:53 +0100

    version de avec Main et le cours sur ModBus

--- src/main/java/fr/btsciel/Main.java
+++ src/main/java/fr/btsciel/Main.java
@@ -1,7 +1,32 @@
 package fr.btsciel;
 
+import fr.btsciel.GestionModbus.ClassModBus;
+import fr.btsciel.GestionModbus.Crc16;
+import fr.btsciel.clavier.In;
+import jssc.SerialPortException;
+
 public class Main {
     public static void main(String[] args) {
+        ClassModBus classModBus;
+        Byte numEsclave;
+        String com;
 
+        System.out.print("quel est le numéro de l'esclave ? : ");
+        numEsclave = In.readByte();
+        System.out.println();
+
+        System.out.print("quel est le com a utiliser ? : ");
+        com = In.readString();
+        System.out.println();
+
+        classModBus = new ClassModBus(numEsclave);
+        try {
+            classModBus.connectEsclave(com, 19200, 8, 1, 2);
+            classModBus.lectureCoils(8192 , 2);
+        } catch (SerialPortException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
